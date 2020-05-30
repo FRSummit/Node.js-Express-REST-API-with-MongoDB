44899d70d6588a4410f430a6619103b678d53673
F R Summit <www.frsummit1@gmail.com>
2020-05-31 00:58:58 +0600

    successfully saved the data to db using postman

--- routes/post.js
+++ routes/post.js
@@ -10,8 +10,39 @@ router.get('/specfic', (req, res) => {
     res.send('Specfic post');
 });
 
-router.post('/', (req, res) => {
-    console.log(req.body);
+// request without async, try, catch
+// router.post('/', (req, res) => {
+//     // console.log(req.body);
+//     // res.send(req.body);
+    
+//     const post = new Post({
+//         title: req.body.title,
+//         description: req.body.description
+//     });
+
+//     post.save()
+//         .then(data => {
+//             res.json(data);
+//         })
+//         .catch(err => {
+//             res.json({ message: err });
+//         })
+//     // res.send(post);
+// });
+
+// request with async, try, catch
+router.post('/', async (req, res) => {
+    const post = new Post({
+        title: req.body.title,
+        description: req.body.description
+    });
+
+    try {
+        const savedPost = await post.save();
+        res.json(savedPost);
+    } catch(err) {
+        res.json({ message: err });
+    }
 });
 
 module.exports = router;
