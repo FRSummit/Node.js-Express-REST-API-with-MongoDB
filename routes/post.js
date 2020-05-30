053ffcedd056d3300c39219b38a9f9343616ae4a
F R Summit <www.frsummit1@gmail.com>
2020-05-31 01:19:47 +0600

    successfully delete a post and updated a post

--- routes/post.js
+++ routes/post.js
@@ -56,4 +56,37 @@ router.post('/', async (req, res) => {
     }
 });
 
+// Get specific post
+router.get('/:postId', async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.postId);
+        res.json(post);
+    } catch(err) {
+        res.json({ message: err });
+    }
+});
+
+// Delete post
+router.delete('/:postId', async (req, res) => {
+    try {
+        const removedPost = await Post.remove({_id: req.params.postId});
+        res.json(removedPost);
+    } catch(err) {
+        res.json({ message: err });
+    }
+});
+
+// Update post
+router.patch('/:postId', async (req, res) => {
+    try {
+        const updatedPost = await Post.updateOne(
+            { _id: req.params.postId },
+            { $set: { title: req.body.title } }
+            );
+        res.json(updatedPost);
+    } catch(err) {
+        res.json({ message: err });
+    }
+});
+
 module.exports = router;
