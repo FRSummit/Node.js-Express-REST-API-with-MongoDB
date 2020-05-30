06ec1fa7995c41bc7342b18ce150635da5c769fa
F R Summit <www.frsummit1@gmail.com>
2020-05-30 23:55:14 +0600

    dotenv, mongoes installed, connected to db

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
-
 const app = express();
+const mongoose = require('mongoose');
+require('dotenv');
 
 //ROUTES
 app.get('/', (req, res) => {
@@ -10,6 +11,13 @@ app.get('/', (req, res) => {
 app.get('/posts', (req, res) => {
     res.send('We are on post');
 });
+
+//Connect to DB
+mongoose.connect(
+    'mongodb+srv://FRSummit:<786648>@restapi-frs-fik92.mongodb.net/test?retryWrites=true&w=majority', 
+    { useNewUrlParser: true }, 
+    () => console.log('Connect to DB!')
+);
 
 //How to we start listening to the server
 app.listen(3000);
